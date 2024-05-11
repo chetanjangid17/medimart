234bd0122857ea3e4af811d83102568d2b6fbc7d
chetanjangid17 <89065235+chetanjangid17@users.noreply.github.com>
2024-05-12 01:09:43 +0530

    done

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,7 +1,6 @@
 import Category from "../../components/category/Category";
 import HeroSection from "../../components/heroSection/HeroSection";
 import HomePageProductCard from "../../components/homePageProductCard/HomePageProductCard";
-import ImageSlider from "../../components/homePageProductCard/Imageslider";
 import Layout from "../../components/layout/Layout";
 import Testimonial from "../../components/testimonial/Testimonial";
 import Track from "../../components/track/Track";
